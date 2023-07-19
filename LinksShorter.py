54dd2e3128c58794ff854df2faba7466c34920b9
Vector <1092381273@qq.com>
2023-07-19 14:09:16 +0800

    Update LinksShorter.py

--- LinksShorter.py
+++ LinksShorter.py
@@ -28,28 +28,13 @@ Available commands:
 /list - List all links
 /update - Update a link
     """
-    bot.reply_to(message, help_message)
-
-
-def start(message):
-    start_message = """
-Welcome to 1uu.me bot! Send /help for commands.
-Available commands:
-/shorten - Shorten a long URL  
-/delete - Delete a shortened URL
-/stats - Get stats for a shortened URL
-/list - List all links
-/update - Update a link
-    """
 
     bot.reply_to(message, start_message)
-
 
 @bot.message_handler(commands=['shorten'])
 def shorten(message):
     msg = bot.reply_to(message, 'Send me the long URL to shorten:')
     bot.register_next_step_handler(msg, process_shorten_step)
-
 
 def process_shorten_step(message):
     url = message.text
@@ -65,12 +50,10 @@ def process_shorten_step(message):
     link = response.json()['link']
     bot.reply_to(message, f'Shortened URL: {link}')
 
-
 @bot.message_handler(commands=['delete'])
 def delete(message):
-    msg = bot.reply_to(message, 'Send me the shortened URL id to delete:')
+    msg = bot.reply_to(message, 'Send me the shortened URL ID to delete:')
     bot.register_next_step_handler(msg, process_delete_step)
-
 
 def process_delete_step(message):
     url = message.text
@@ -89,12 +72,10 @@ def process_delete_step(message):
     else:
         bot.reply_to(message, 'Error deleting URL.')
 
-
 @bot.message_handler(commands=['stats'])
 def stats(message):
-    msg = bot.reply_to(message, 'Send me the shortened URL to get stats:')
+    msg = bot.reply_to(message, 'Send me the shortened URL ID to get stats:')
     bot.register_next_step_handler(msg, process_stats_step)
-
 
 def process_stats_step(message):
     url = message.text
@@ -110,71 +91,91 @@ def process_stats_step(message):
 
     data = response.json()
 
-    message = f"""
-Stats for {url}:   
+    stats_message = f"""
+Stats for {url}:
 
-Total clicks: {data['visit_count']}  
+Total clicks: {data['visit_count']}
 
-All time:
-  Top browsers: {format_top_stats(data['allTime']['browser'])}
-  Top countries: {format_top_stats(data['allTime']['country'])}
+Updated At: {data['updatedAt']}
+Address: {data['address']}  
+Banned: {data['banned']}
+Created At: {data['created_at']}
+ID: {data['id']}
+Link: `{data['link']}`
+Password: {data['password']}
+Target: `{data['target']}`
+Updated At: {data['updated_at']}
+  """
 
-Last 7 days:
-  Top browsers: {format_top_stats(data['lastWeek']['browser'])}
-  Top countries: {format_top_stats(data['lastWeek']['country'])}
-    """
-    bot.reply_to(message, message)
+#All time:
+#  Top browsers: {format_top_stats(data['allTime']['stats']['browser'])}
+#  Top countries: {format_top_stats(data['allTime']['stats']['country'])}
 
+#Last 7 days:
+#  Top browsers: {format_top_stats(data['lastWeek']['stats']['browser'])}
+#  Top countries: {format_top_stats(data['lastWeek']['stats']['country'])}
+
+    bot.send_message(chat_id=message.chat.id, text=stats_message, parse_mode='Markdown')
 
 def format_top_stats(stats):
-    top = sorted(stats, key=lambda x: x['value'], reverse=True)[:3]
-    return ', '.join([f"{x['name']} ({x['value']})" for x in top])
 
+  top = sorted(stats, key=lambda x: x['value'], reverse=True)[:3]
+
+  return ', '.join([f"{x['name']} ({x['value']})" for x in top])
 
 @bot.message_handler(commands=['list'])
 def list_links(message):
-    response = requests.get(f'{API_URL}/links', headers={'X-API-KEY': API_KEY})
 
-    data = response.json()
+  response = requests.get(f'{API_URL}/links', headers={'X-API-KEY': API_KEY})
 
-    links_message = "Your links: \n"
+  data = response.json()
 
-    i = 1
-    for link in data['data']:
-        links_message += f"No. {i}:\n"
-        links_message += f"ID: `{link['id']}`\n"
-        links_message += f"Target: `{link['target']}`\n"
-        links_message += f"Link: `{link['link']}`\n"
+  links_message = "Your links: \n"
 
-        i += 1
+  i = 1
+  for link in data['data']:
+    links_message += f"No. {i}:\n"
+    links_message += f"ID: `{link['id']}`\n"
+    links_message += f"Target: `{link['target']}`\n"
+    links_message += f"Link: `{link['link']}`\n"
 
-    bot.send_message(chat_id=message.chat.id, text=links_message, parse_mode='Markdown')
+    i += 1
 
+  bot.send_message(chat_id=message.chat.id, text=links_message, parse_mode='Markdown')
 
 @bot.message_handler(commands=['update'])
 def update(message):
-    msg = bot.reply_to(message, 'Send me the link ID to update:')
-    bot.register_next_step_handler(msg, process_update_step)
-
+  msg = bot.reply_to(message, 'Send me the link ID to update:')
+  bot.register_next_step_handler(msg, process_update_step)
 
 def process_update_step(message):
-    link_id = message.text
-    msg = bot.reply_to(message, 'Send me the new target URL:')
-    bot.register_next_step_handler(msg, process_update_url_step, link_id)
-
+  link_id = message.text
+  msg = bot.reply_to(message, 'Send me the new target URL:')
+  bot.register_next_step_handler(msg, process_update_url_step, link_id)
 
 def process_update_url_step(message, link_id):
-    url = message.text
-    headers = {'X-API-KEY': API_KEY}
-    data = {'target': url}
+  url = message.text
+  msg = bot.reply_to(message, 'Send me the new address:')
+  bot.register_next_step_handler(msg, process_update_address_step, link_id, url)
 
-    response = requests.patch(f'{API_URL}/links/{link_id}', headers=headers, json=data)
+def process_update_address_step(message, link_id, url):
+  address = message.text
+  headers = {'X-API-KEY': API_KEY}
+  data = {
+    'target': url,
+    'address': address
+  }
 
-    if response.status_code == 200:
-        bot.reply_to(message, 'Link updated successfully!')
-    else:
-        bot.reply_to(message, 'Error updating link')
+  response = requests.patch(f'{API_URL}/links/{link_id}', headers=headers, json=data)
 
+  if response.status_code == 200:
+    new_link = response.json()['link']
+    msg = f'Link updated successfully! New link: `{new_link}`'
+    bot.send_message(chat_id=message.chat.id, text=msg, parse_mode='Markdown')
+  else:
+    bot.reply_to(message, 'Error updating link!')
 
 if __name__ == '__main__':
+    logger.debug('%s bot started', time.asctime())
     bot.polling()
+    logger.debug('%s bot stopped', time.asctime())
